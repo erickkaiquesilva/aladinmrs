f71b0de8bb3aece28ef8c131652ecc470efc4175
erickkaiquesilva <erickkay272@gmail.com>
2025-02-10 18:48:33 -0300

    [feat: loginView] Create: loginView, ContainerBoxServiceView and ServiceBoxView, adjust nomenclature at tokens space.

--- aladin/Commons/Space.swift
+++ aladin/Commons/Space.swift
@@ -1,8 +1,12 @@
 import Foundation
 
 enum Space: CGFloat {
+
+    /// Extra small spacing (4pt)
+    case xxSmall = 4.0
+
     /// Extra small spacing (8pt)
-    case extraSmall = 8.0
+    case xSmall = 8.0
 
     /// Small spacing (16pt)
     case small = 16.0
@@ -14,38 +18,38 @@ enum Space: CGFloat {
     case large = 32.0
 
     /// Extra large spacing (40pt)
-    case extraLarge = 40.0
+    case xLarge = 40.0
 
     /// 2x Extra large spacing (48pt)
-    case x2ExtraLarge = 48.0
+    case xxLarge = 48.0
 
     /// 3x Extra large spacing (56pt)
-    case x3ExtraLarge = 56.0
+    case xxxLarge = 56.0
 
     /// 4x Extra large spacing (64pt)
-    case x4ExtraLarge = 64.0
+    case huge = 64.0
 
     /// 5x Extra large spacing (72pt)
-    case x5ExtraLarge = 72.0
+    case xHuge = 72.0
 
     /// 6x Extra large spacing (80pt)
-    case x6ExtraLarge = 80.0
+    case xxHuge = 80.0
 
     /// 7x Extra large spacing (88pt)
-    case x7ExtraLarge = 88.0
+    case xxxHuge = 88.0
 
     /// 8x Extra large spacing (96pt)
-    case x8ExtraLarge = 96.0
+    case giant = 96.0
 
     /// 9x Extra large spacing (104pt)
-    case x9ExtraLarge = 104.0
+    case xGiant = 104.0
 
     /// 10x Extra large spacing (112pt)
-    case x10ExtraLarge = 112.0
+    case xxGiant = 112.0
 
     /// 11x Extra large spacing (120pt)
-    case x11ExtraLarge = 120.0
+    case xxxGiant = 120.0
 
     /// 12x Extra large spacing (128pt)
-    case x12ExtraLarge = 128.0
+    case xxxxGiant = 128.0
 }
--- aladin/Scenes/Login/Views/LoginView.swift
+++ aladin/Scenes/Login/Views/LoginView.swift
@@ -1,13 +1,6 @@
-//
-//  ContentView.swift
-//  aladin
-//
-//  Created by Magalu on 10/02/25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,6 +12,6 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    LoginView()
+//}
