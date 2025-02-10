de3a60bd5037f480165bc3d7fb0467cd8b28bc2a
erickkaiquesilva <erickkay272@gmail.com>
2025-02-10 18:59:42 -0300

    [feat: loginView] Create helper for capture window and update color.

--- aladin/Scenes/Login/Views/LoginView.swift
+++ aladin/Scenes/Login/Views/LoginView.swift
@@ -1,14 +1,57 @@
 import SwiftUI
 
 struct LoginView: View {
+
+    @StateObject private var viewModel: /*TODO LoginViewModel*/
+    @State private var inputToken: String = ""
+    private var backgroundColor: Color {
+        switch viewModel.repositoryType {
+        case .bitbucket:
+            return .bitbucketBackground
+        case .gitlab:
+            return .gitlabBackground
+        case .github:
+            return .githubBackground
+        case .none:
+            return .backgroundPrimary
+        }
+    }
+
+    init(viewModel: LoginViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Aladin Merge's")
+                .font(.largeTitle)
+                .foregroundStyle(.fontColorPrimary)
+            if !viewModel.isLoading {
+                ContainerServiceBoxView(
+                    repositoryType: viewModel.repositoryType,
+                    isSelected: viewModel.isSelectedRepository,
+                    didSelected: viewModel.seletedRepository,
+                    didDeselected: viewModel.deselectedRepository,
+                    didConfirme: viewModel.authentication
+                )
+            }
+
+            if viewModel.isLoading {
+                /// TODO implementation component loading
+            }
+
+            if viewModel.errorState {
+                /// TODO implementation component erro
+            }
         }
         .padding()
+        .background(WindowCapturer())
+        .onChange(of: viewModel.repositoryType) {
+            WindowManager.setWindowBackground(color: backgroundColor)
+        }
+        .onAppear {
+            WindowManager.setWindowBackground(color: .backgroundPrimary)
+        }
     }
 }
 
