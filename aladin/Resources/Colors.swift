4a5ebd69c95479a3b76a4c17a07d2cdb52998c55
erickkaiquesilva <erickkay272@gmail.com>
2025-02-10 17:49:25 -0300

    [feat: configAssets] add colors default for project.

---
+++ aladin/Resources/Colors.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct Colors {
+
+    // MARK: - Backgrounds
+    static let backgroundPrimary: Color = Color("primary")
+    static let backgroundSecondary: Color = Color("secondary")
+    static let backgroundTertiary: Color = Color("tertiary")
+    static let backgroundGitHub: Color = Color("github_background")
+    static let backgroundGitLab: Color = Color("gitlab_background")
+    static let backgroundBitbucket: Color = Color("bitbucket_background")
+}
