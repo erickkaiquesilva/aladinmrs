997ea09528fb096d179e54a53d9845fd3ecce5cc
erickkaiquesilva <erickkay272@gmail.com>
2025-02-10 18:04:56 -0300

    [feat: configAssets] fix name colors default project, and add font.

--- aladin/Resources/Colors.swift
+++ aladin/Resources/Colors.swift
@@ -3,9 +3,9 @@ import SwiftUI
 struct Colors {
 
     // MARK: - Backgrounds
-    static let backgroundPrimary: Color = Color("primary")
-    static let backgroundSecondary: Color = Color("secondary")
-    static let backgroundTertiary: Color = Color("tertiary")
+    static let backgroundPrimary: Color = Color("colorPrimary")
+    static let backgroundSecondary: Color = Color("colorSecondary")
+    static let backgroundTertiary: Color = Color("colorTertiary")
     static let backgroundGitHub: Color = Color("github_background")
     static let backgroundGitLab: Color = Color("gitlab_background")
     static let backgroundBitbucket: Color = Color("bitbucket_background")
